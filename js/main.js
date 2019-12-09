2061c16aba3773066b67f0e194bce1b1ef52f551
Jordan Trickett <jordan1trickett@gmail.com>
2019-12-09 15:17:06 +1100

    game board clickable, populates and aternates between x and 0

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
 $(document).ready( function () {
   console.log('DOM LOADED');
 
+  let player = 1;
+
+
+  $('.squares').on('click', function (ev) {
+    console.log(ev);
+
+    const squareClicked = '#' + ev.target.id;
+
+    console.log(squareClicked);
+
+    if (player === 1) {
+
+      const $counterZero = $('<div>0</div>');
+
+      $counterZero.addClass('counterZero');
+
+      $(squareClicked).append( $counterZero );
+
+      // $(squareClicked).html('<div>0</div>').addClass('.counterZero').append()
+      //
+
+      //add the css
+
+      // $('.counterZero').css('visibility', 'visible');
+      player = 2;
+    } else {
+
+      const $counterX = $('<div>X</div>');
+
+      $counterX.addClass('counterX');
+
+      $(squareClicked).append( $counterX );
+
+
+      // $(squareClicked).html('<div>X</div>').addClass('.counterZero').append()
+
+
+      // $('.counterX').css('visibility', 'visible');
+
+
+      player = 1;
+    };
+
+  });
 
 
 
 
 
 
-  
 }); //DOM Loaded

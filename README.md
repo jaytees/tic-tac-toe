5544914576272fa60f3d601951872af4e4b9fae3
Jordan Trickett <jordan1trickett@gmail.com>
2020-04-07 22:46:00 +0100

    readme

--- README.md
+++ README.md
@@ -1,56 +1,87 @@
-# Project 0
-
 # Tic - Tac - Toe
 
-**Overview**
+Classic game with an unbeatable, semi intelligent AI.
 
-* Build a web application from scratch, without a starter codebase.
-* Use the past 2 weeks programming skills to map out the game logic.
-* Separate HTML, CSS, and JavaScript files.
-* Build the application to the spec provided.
-* A dynamic game that allows two players to compete.
-Craft a README.md file that explains your app to the world.
+##Brief
 
-*and finally...*
+- Render a game board in the browser
+- Switch turns between X and O (or whichever markers you select); your game should prevent users from playing a turn into a square that is already occupied
+- Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
+- Include separate HTML / CSS / JavaScript files
+- Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
+- Use Javascript with jQuery for DOM manipulation
+- Deploy your game online, where the rest of the world can access it
+- Use semantic markup for HTML and CSS (adhere to best practices)
+
+**and finally...**
 
 **have some fun with it!!**
 
-**Technical Requirements**
+##Tech Used
 
-The app must;
+- HTML
+- CSS
+- Vanilla JavaScript
+- jQuery
 
-* Render a game board in the browser
-* Switch turns between X and O (or whichever markers you select); your game should prevent users from playing a turn into a square that is already occupied
-* Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
-* Include separate HTML / CSS / JavaScript files
-* Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
-* Use Javascript with jQuery for DOM manipulation
-* Deploy your game online, where the rest of the world can access it
-* Use semantic markup for HTML and CSS (adhere to best practices)
+##Link to the Deployed Game
 
+https://jaytees.github.io/tic-tac-toe/
+
+##Experience and Process
+
+- This was my first project at General Assembly and therefore my first website and it really was such an exciting and fulfilling experience. Every feature I got working came with a huge sense of achievement.
+- Being my first project I had really low expectations, so I broke this process down into the smallest little blocks imaginable, wrote lots of pseudo code and worked through that list until the time ran out. Learning a valuable lesson about how to approach a problem in the process.
+- Just to give some perspective these blocks began with render a the board to the screen, then can I attach a click handler to the squares that console logs the div id.
+- And somewhere along the way I just hit a flow and started ticking chunks off my list daily and adding more functionality onto the end.
+- This project definitely gave oxygen to fire that was already catching.
+
+##Wins
+
+- **AI** - My proudest achievement on this project. The AI will look to block any possible user wins and look for a win. This wasn't on my radar when I first started this project but soon became the goal. Once I had developed the code to check if a user had won, I realised I could use the same logic to integrate a semi-intelligent AI. So again, I broke the process down into the smallest blocks as follows;
+  - AI placing counters on random grid squares.
+  - Use the same logic to check if a user has won, to check if a user is about to win.
+  - Use that same logic to check if there is a winning combination available and play that square.
+- **Seperation of Concerns** - The separation of concern between UI and game logic was a core goal of mine from the offset. As I mentioned I had low expectations going into this so my goal was to adhere to core principles of good, clean code.
+
+##Challenges
+
+- **Checking for a Win** - I found it difficult to work out how to articulate the winning combinations in a way that could be checked against the current board status. I knew this could be achieved with an array storing the current board status and a long if statement comparing this to the possible winning combinations. But knew this wasn't the DRYest or simplest way. After some trial and error and research I came across 2D arrays. Which would allow me to store each winning combo, loop through them and evaluate the current board status with the same conditional.
+
+##Code
+
+**Win Logic**
+
+![win logic function](images/win-logic-function.png)
+
+![win logic arrays](images/win-logic-arrays.png)
+
+**AI**
+
+- Checking for blocks or wins
+
+![ai win](images/win-or-block1.png)
+
+![ai block](images/win-or-block2.png)
+
+**Winning Counters**
+
+- Animate the winning counters
+
+![animate string](images/animate1.png)
+
+![animate function](images/animate2.png)
+
+##Screenshots
 
 **Welcome Screen Image**
 
 ![welcome screen image](images/welcome-screen.png)
 
-
 **Game Screen Image**
 
 ![game screen image](images/game-play.png)
 
-**Link to Game**
+##Current Bugs
 
-[TIC-TAC-TOE](https://jaytees.github.io/tic-tac-toe/)
-
-
-**Tech Used**
-
-* HTML
-* CSS
-* Vanilla JavaScript
-* jQuery
-
-
-**Current Bugs**
-
-* The majority of the bugs currently exist in single player mode. You can currently register false by continuously clicking and forcing a draw. This also causes the ai the make multiple moves.
+- You can currently register moves by continuously clicking free squares and forcing a draw.
--- js/main-presentation-copy.js
+++ js/main-presentation-copy.js
@@ -1,177 +1,136 @@
-$(document).ready( function () {
-  console.log('DOM LOADED-presentation');
+$(document).ready(function() {
+  console.log("DOM LOADED-presentation");
 
-
-
-
-  $('#singlePlayer').on('click', function () {
-
+  $("#singlePlayer").on("click", function() {
     gameType = 1;
-    $('.welcomeScreen').css('visibility', 'hidden');
-
+    $(".welcomeScreen").css("visibility", "hidden");
   }); //singlePlayer button
 
-  $('#multiPlayer').on('click', function () {
-
+  $("#multiPlayer").on("click", function() {
     gameType = 2;
-    $('.welcomeScreen').css('visibility', 'hidden');
-
+    $(".welcomeScreen").css("visibility", "hidden");
   }); //multiPlayer button
 
-
-  $('.squares').on('click', function (ev) {
+  $(".squares").on("click", function(ev) {
     // console.log(ev);
 
     playerIdentifier(player); //changes scoreboard color
 
-    const squareClicked = '#' + ev.delegateTarget.id;
+    const squareClicked = "#" + ev.delegateTarget.id;
     //provides div position of click, using jQuery object
 
-    const ifTaken = squareChecker( squareClicked );
-      // runs function to check if square already played
+    const ifTaken = squareChecker(squareClicked);
+    // runs function to check if square already played
 
+    if (ifTaken === false && winner === false) {
+      //checks square available to play and no winner
 
+      $("#message").text(""); //clear any messages
 
-    if (ifTaken === false && winner === false) { //checks square available to play and no winner
+      if (player === 1) {
+        const $counterZero = $('<div class="counterZero">O</div>');
+        $(squareClicked).append($counterZero);
+        //creates div with O
 
-        $('#message').text(''); //clear any messages
+        gameLogic(ev.delegateTarget.id, "O");
 
-            if (player === 1) {
+        winChecker("O");
 
-              const $counterZero = $('<div class="counterZero">O</div>');
-              $(squareClicked).append($counterZero);
-              //creates div with O
+        // squareSelectAndWin(ev.delegateTarget.id, 'O')
 
-              gameLogic(ev.delegateTarget.id, 'O');
+        player = 2;
+      } else {
+        const $counterX = $("<div class=counterX>X</div>");
+        $(squareClicked).append($counterX);
+        //creates div with X
 
-              winChecker('O');
+        gameLogic(ev.delegateTarget.id, "X");
 
-          // squareSelectAndWin(ev.delegateTarget.id, 'O')
+        winChecker("X");
 
-              player = 2;
+        // squareSelectAndWin(ev.delegateTarget.id, 'X')
 
-            } else {
+        player = 1;
+      } //nested if
 
-               const $counterX = $('<div class=counterX>X</div>');
-               $(squareClicked).append( $counterX );
-               //creates div with X
-
-               gameLogic(ev.delegateTarget.id, 'X');
-
-               winChecker('X');
-
-          // squareSelectAndWin(ev.delegateTarget.id, 'X')
-
-              player = 1;
-
-
-            }; //nested if
-
-          moveCount();
-
-      } else if (winner === true) {
-
-        $('#message').text('PLEASE RESET BOARD TO PLAY AGAIN! ').css({
-          color: '#ff5454',
-          visibility: 'visible',
-          'font-size': '17px',
-          'letter-spacing': '4px',
-          'line-height': '24px',
+      moveCount();
+    } else if (winner === true) {
+      $("#message")
+        .text("PLEASE RESET BOARD TO PLAY AGAIN! ")
+        .css({
+          color: "#ff5454",
+          visibility: "visible",
+          "font-size": "17px",
+          "letter-spacing": "4px",
+          "line-height": "24px"
         });
-
-
-
-    }; //if
-
+    } //if
 
     // for one player
 
     if (gameType === 1 && winner === false && moves !== 9) {
-
-
-      setTimeout(function () {
-
+      setTimeout(function() {
         playerIdentifier(player); //changes scoreboard color
         gameLogicAi();
-        winChecker('X');
+        winChecker("X");
         moveCount();
         // squareSelectAndWin(ev.delegateTarget.id, 'X')
         player = 1;
-
       }, 2000);
-
-
-
-    }; //one player
-
-
-
+    } //one player
   }); //square selector
 
-
-  const squareChecker =  function ( squareClicked ) {
-
-
+  const squareChecker = function(squareClicked) {
     const contents = $(squareClicked).text();
 
-      if (contents === 'O' || contents === 'X') {
-
-        $('#message').text('PLEASE SELECT ANOTHER SQUARE').css({
-          color: '#ff5454',
-          visibility: 'visible',
-          'font-size': '17px',
-          'letter-spacing': '2px',
-          'line-height': '24px',
+    if (contents === "O" || contents === "X") {
+      $("#message")
+        .text("PLEASE SELECT ANOTHER SQUARE")
+        .css({
+          color: "#ff5454",
+          visibility: "visible",
+          "font-size": "17px",
+          "letter-spacing": "2px",
+          "line-height": "24px"
         });
 
-        return true;
-
-      } else {
-
-        return false;
-
-      }; //if
-
+      return true;
+    } else {
+      return false;
+    } //if
   }; //square checker
 
-
-  const moveCount = function () {
-
+  const moveCount = function() {
     moves += 1;
     console.log(moves);
 
     if (moves === 9 && winner === false) {
-      $('#message').text('DRAW!').css({
-        color: '#ff5454',
-        visibility: 'visible',
-        'font-size': '17px',
-        'letter-spacing': '2px',
-        'line-height': '24px',
-      });
+      $("#message")
+        .text("DRAW!")
+        .css({
+          color: "#ff5454",
+          visibility: "visible",
+          "font-size": "17px",
+          "letter-spacing": "2px",
+          "line-height": "24px"
+        });
 
       // console.log('gameover');
 
-      setTimeout(function () {
-
-        $('#resetButton').trigger('click');
+      setTimeout(function() {
+        $("#resetButton").trigger("click");
 
         // .fadeOut("slow")
-
       }, 3000);
 
       return true;
-
-    };
-
-
+    }
   }; //moves checker
 
+  $("#resetButton").on("click", function() {
+    $(".counterZero, .counterX").remove();
 
-  $('#resetButton').on('click', function () {
-
-    $('.counterZero, .counterX').remove();
-
-    grid = ['', '', '', '', '', '', '', ''];
+    grid = ["", "", "", "", "", "", "", ""];
 
     winner = false;
 
@@ -181,114 +140,96 @@ $(document).ready( function () {
 
     winningComboFound = false;
 
-    $('#message').text('');
+    $("#message").text("");
     //3 lines below with empty values reset css to default
-    $('.scoreBoard').css('color', '');
-    $('#x').css('color', '');
-    $('#o').css('color', '');
+    $(".scoreBoard").css("color", "");
+    $("#x").css("color", "");
+    $("#o").css("color", "");
 
     // console.log(player);
-
-
   }); // resetButton
 
-
-  const winChecker = function (counter) {
-
+  const winChecker = function(counter) {
     if (winLogic(counter)) {
       //if win logic returns true, winner found
 
       winner = true;
 
-      boardWinAnimate( winningSelector, counter);
+      boardWinAnimate(winningSelector, counter);
 
-        if (counter === 'O') {
+      if (counter === "O") {
+        // console.log('O is Winner');
+        scoreO += 1;
+        $("#scoreCountO").text(scoreO); //updates score on screen
+        $("#message")
+          .text("O IS WINNER!")
+          .css({
+            color: "#5490ff",
+            visibility: "visible"
+          }); //displays message
+        $("#scoreBoardLeft").css("color", "#5490ff"); //makes score board all blue
+        $("#o").css("color", "#5490ff");
+        $("#x").css("color", "#d6d6d6");
+      } else if (counter === "X") {
+        // console.log('X is Winner');
+        scoreX += 1;
+        $("#scoreCountX").text(scoreX);
+        $("#message")
+          .text("X IS WINNER!")
+          .css({
+            color: "#5b9665",
+            visibility: "visible"
+          });
+        $("#scoreBoardRight").css("color", "#5b9665"); //makes score board all green
+        $("#x").css("color", "#5b9665");
+        $("#o").css("color", "#d6d6d6");
+      } // nested if
 
-            // console.log('O is Winner');
-            scoreO += 1;
-            $('#scoreCountO').text(scoreO); //updates score on screen
-            $('#message').text('O IS WINNER!').css({
-              color: '#5490ff',
-              visibility: 'visible',
-            }); //displays message
-            $('#scoreBoardLeft').css('color', '#5490ff'); //makes score board all blue
-            $('#o').css('color', '#5490ff');
-            $('#x').css('color', '#d6d6d6');
+      setTimeout(function() {
+        $("#resetButton").trigger("click");
 
-
-
-        } else if (counter === 'X') {
-
-            // console.log('X is Winner');
-            scoreX += 1;
-            $('#scoreCountX').text(scoreX);
-            $('#message').text('X IS WINNER!').css({
-              color: '#5b9665',
-              visibility: 'visible',
-            });
-            $('#scoreBoardRight').css('color', '#5b9665'); //makes score board all green
-            $('#x').css('color', '#5b9665');
-            $('#o').css('color', '#d6d6d6');
-
-
-        }; // nested if
-
-        setTimeout(function () {
-
-          $('#resetButton').trigger('click');
-
-          // .fadeOut("slow")
-
-        }, 3000);
-
-    }; // if
-
-
-
+        // .fadeOut("slow")
+      }, 3000);
+    } // if
   }; //winChecker
 
+  const playerIdentifier = function(player) {
+    if (player === 1) {
+      //statements are opposite to turn logic so it alternates after every squareClicked
 
-  const playerIdentifier = function ( player ) {
-
-      if (player === 1) {  //statements are opposite to turn logic so it alternates after every squareClicked
-
-        $('#x').css('color', '#5b9665');
-        $('#o').css('color', '#d6d6d6');
-
-      } else {
-
-        $('#o').css('color', '#5490ff');
-        $('#x').css('color', '#d6d6d6');
-
-      }; // if
-
-
+      $("#x").css("color", "#5b9665");
+      $("#o").css("color", "#d6d6d6");
+    } else {
+      $("#o").css("color", "#5490ff");
+      $("#x").css("color", "#d6d6d6");
+    } // if
   }; // playerIdentifier
 
-
-  const boardWinAnimate = function ( winSelector, counter ) {
-
+  const boardWinAnimate = function(winSelector, counter) {
     // console.log(winningCounter0, winningCounter1, winningCounter2);
 
-    let color = '#5490ff';
+    let color = "#5490ff";
 
-    if (counter === 'X') {
-
-      color = '#5b9665';
-
-    };
+    if (counter === "X") {
+      color = "#5b9665";
+    }
 
     // let color = (counter === 'X') ? '#5490ff' : '#5b9665';
 
-    $( winSelector ).children().css( 'color', color )
-    .animate({
-        fontSize: '65px',
-      }, 1000).animate({
-        fontSize: '55px'
-      }, 1000);
-
+    $(winSelector)
+      .children()
+      .css("color", color)
+      .animate(
+        {
+          fontSize: "65px"
+        },
+        1000
+      )
+      .animate(
+        {
+          fontSize: "55px"
+        },
+        1000
+      );
   }; // boardWinAnimate
-
-
-
 }); //DOM Loaded
